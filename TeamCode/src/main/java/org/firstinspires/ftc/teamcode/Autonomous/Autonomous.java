7305afbf9e28956c1806611e93f7b4a0168713db
Pietro Cartaxo Villas Bôas <pietrocartaxovillasboas@gmail.com>
2026-09-03 13:52:15 -0300

    a

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Autonomous.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Autonomous", group = "Autonomous")
+public class Autonomous extends OpMode {
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
